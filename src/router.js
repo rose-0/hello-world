81666bfbf3b2d7b1b14bfa0d8980e286f5947499
rose-0 <1784510335@qq.com>
2020-11-15 22:25:42 +0800

    完成数据表

--- src/router.js
+++ src/router.js
@@ -12,6 +12,7 @@ import Params from './components/goods/Params.vue'
 import List from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/report.vue'
 Vue.use(Router)
 
 //export default new Router直接new然后导出
@@ -36,6 +37,7 @@ const router = new Router({
       {path:'/goods',component:List},
       {path:'/goods/add',component:Add},
       {path:'/orders',component:Order},
+      {path:'/reports',component:Report},
       ] 
     }
   ]
